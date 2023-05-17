35956db51d4097233336e5a5c2481c1affc5d32c
김태수 <65998694+Blakekim93@users.noreply.github.com>
2023-05-17 09:56:09 +0900

    Time: 241 ms (5.02%), Space: 20.6 MB (28.06%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,8 +3,4 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        max_index = len(s) - 1
-        for i, v in enumerate(s):
-            if i > max_index / 2:
-                break
-            s[i], s[max_index-i] = s[max_index-i], s[i]
+        s.reverse()
