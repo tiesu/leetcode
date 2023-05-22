311d7e34e2cf95db4dedf0acfcfd9af72147ad92
Taesu Kim <65998694+tiesu@users.noreply.github.com>
2023-05-22 18:23:11 +0900

    Time: 5548 ms (10.19%), Space: 17.1 MB (32.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,20 @@
-import itertools
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        coms = itertools.combinations(nums, 2)
-        for com in coms:
-            if sum(com) == target:
-                index_first = nums.index(com[0])
-                return [index_first, nums.index(com[1], index_first+1)]
+        combi = itertools.combinations(nums, 2)
+        result = ''
+        for i in combi:
+            if sum(i) == target:
+                result = i
+                break
+        
+        result_1 = ''
+        result_2 = ''
+        
+        for i, v in enumerate(nums):
+            if v == result[0] and result_1 == '':
+                result_1 = i
+            if v == result[1] and result_1 != i and result_2 == '':
+                result_2 = i
+                
+        return [result_1, result_2]
+                
